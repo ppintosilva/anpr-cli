f88c665e2803b86459ce1e1e2f84f6543257ab4e
Pedro Pinto da Silva <ppintodasilva@gmail.com>
2019-06-28 14:24:32 +0100

    Updated help and options for command wrangle cameras

--- cli/wrangle/cameras.py
+++ cli/wrangle/cameras.py
@@ -6,7 +6,7 @@ import pandas as pd
 
 @click.argument(
     'output',
-    type = click.File('wb'),
+    type = str,
 )
 @click.argument(
     'input-csv',
@@ -17,23 +17,24 @@ import pandas as pd
     type=click.Choice(['geojson', 'csv']),
     default = 'geojson',
     required = False,
-    help = "Format of the output file containing the wrangled cameras dataset"
+    help = "Format of the output file with the wrangled cameras dataset"
 )
 @click.option(
     '--names',
     type = str,
-    default = 'id,lat,lon,name,description,is_commissioned,type',
+    default = None,
     required = False,
-    help = "Role of each of the columns in the input csv file"
+    help = "Names of columns in the input csv file"
 )
 @click.option(
-    '--header/--no-header',
-    default = True,
+    '--skip-lines',
+    default = 0,
+    type = int,
     required = False,
-    help = "Whether the first line of the csv file should be ignored or not."
+    help = "Number of lines to skip at the start of the file."
 )
 @click.command()
-def cameras(input_csv, output, output_format, names, header):
+def cameras(input_csv, output, output_format, names, skip_lines):
     """
     Wrangle a raw dataset of ANPR cameras.
 
@@ -44,32 +45,63 @@ def cameras(input_csv, output, output_format, names, header):
     information from text-based descriptions of the cameras, and assigns it to
     new columns. Furthermore, cameras that may not be relevant are filtered
     out: decomissioned cameras, test cameras and cameras located in car parks.
-    Use the --names option to specify the role of each column in the input csv
-    file. Specify these a comma separated string of values. The following
-    values are recognised: id,name,lat,lon,description,is_commissioned.
-    Additional columns will be ignored but kept on the resulting dataframe.
-    The description columns is used to infer the following attributes:
-    direction, is_carpark, address, road_category. Name is used to infer
-    whether the camera is a test camera or not. Car park cameras and
+
+    By default, column names are retrieved from the first line of the file.
+    The following column names are mandatory: 'id', 'lat' and 'lon'. Either
+    edit the column names directly in the input csv file or specify these as a
+    comma separated string of values via the --names option. Additional
+    recognised column names are: 'name', 'description' and 'is_commissioned'.
+    Any additional columns will be ignored but kept on the resulting dataframe.
+    The 'description' column is used to infer the following attributes:
+    direction, is_carpark, address, road_category. The 'name' column is used to
+    infer whether the camera is a test camera or not. Car park cameras and
     decommissioned cameras are removed.
 
     Example usage:
 
     \b
-    anpr wrangle cameras \\
-        --names "id,name,lat,lon,description,is_commissioned,other_col" \\
-        raw_cameras.csv \\
-        wrangled_cameras.geojson
+        (1) - Using header from file:
 
-    Note that names specifies the new column names of the dataframe by role.
-    Any pre-existing column names are ignored.
+    \b
+        anpr wrangle cameras raw_cameras.csv wrangled_cameras.geojson
+
+    \b
+        (2) - Specyfing column names via the --names option:
+
+    \b
+        anpr wrangle cameras \\
+            --names "id,name,lat,lon,description,is_commissioned,other_col" \\
+            --skip-lines 1 \\
+            raw_cameras.csv \\
+            wrangled_cameras.geojson
 
     If the output format is GeoJSON (default), the cameras coordinates are
     projected into UTM coordinates. Working with UTM coordinates is useful
     further down on the processing pipeline when cameras are mapped onto the
     road network (see command wrangle merge-cameras).
+
+    This script uses anprx to wrangle the cameras dataset. For more
+    fine-grained control over the behavior of this function please consider
+    using the python library: https://github.com/ppintosilva/anprx
     """
-    col_names = names.split(',')
+
+    cameras = pd.read_csv(
+        filepath_or_buffer = input_csv,
+        sep    = ',',
+        names  = names.split(',') if names else None,
+        header = None if names else 0,
+        skiprows = skip_lines,
+        dtype  = {
+            "id": object,
+            "name": object,
+            "lat": np.float64,
+            "lon": np.float64,
+            "is_commissioned" : bool,
+            "description" : object
+        }
+    )
+
+    col_names = names.split(',') if names else cameras.columns.values
 
     has_description = ('description' in col_names)
 
@@ -79,24 +111,6 @@ def cameras(input_csv, output, output_format, names, header):
     extract_road_category     = 'description' if has_description else False
     drop_is_test              = 'name' if ('name' in col_names) else False
     drop_is_not_commissioned  = ('is_commissioned' in col_names)
-
-    click.echo(col_names)
-
-    cameras = pd.read_csv(
-        filepath_or_buffer = input_csv,
-        sep    = ',',
-        names  = col_names,
-        header = 1 if header else None,
-        dtype  = {
-            "id": object,
-            "name": object,
-            "lat": np.float64,
-            "lon": np.float64,
-            "is_commissioned" : object,
-            "description" : object
-        }
-    )
-    click.echo(cameras.head())
 
     proj_coords = (output_format == "geojson")
 
